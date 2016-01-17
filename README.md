72744ea7138eac8ad6520473f2620e88e0b54727
Himansu Desai <himan1.desai@samsung.com>
2016-01-17 14:01:44 -0800

    first cut of jquilar library. README file and support for 2 widgets viz. slider and datepicker

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # jquilar.js
+
+jquilar.js is a jquery-ui wrapper library for Angular2.
+
+If 3rd party libraries with an imperative API can be wrapped as Angular2 components with a more declarative API, then it cleans up the application structure.
+
+Everything continues to be components within components with data binding
+
+Currently tested with:
+- Angular 2.0.0-beta.0
+- jQuery UI 1.11.4
+- jQuery 1.10.2
+
+### Supported Widgets (more coming soon)
+- Slider
+- Date Picker
+
+
+The examples can be viewed online at himansudesai.github.com/jquilar
---
+++ lib/jquilar.ts
@@ -0,0 +1,102 @@
+import { Component, EventEmitter } from "angular2/core";
+import { OnChanges, SimpleChange, OnInit, AfterContentInit, AfterViewInit, OnDestroy, ElementRef } from 'angular2/core';
+import { Inject } from 'angular2/core';
+
+// jquery-ui slider
+@Component({
+  selector: 'jquilar-slider',
+  inputs: ['val', 'orientation', 'step'],
+  events: ['stop'],
+  template: '<div class="jquilar-slider"></div>'
+})
+
+export class jqUIlarSlider {
+  val: number;
+  stop: EventEmitter<number>;
+  domElement: any;
+  slider: any;
+  orientation: string;
+  step: number;
+
+  constructor( @Inject(ElementRef) elementRef: ElementRef) {
+    this.domElement = elementRef.nativeElement;
+    this.stop = new EventEmitter();
+    this.val = 0;
+    this.slider = undefined;
+    this.orientation = 'horizontal';
+    this.step = 1;
+  }
+
+  ngAfterContentInit() {
+  }
+
+  ngOnChanges(changes: {[propertyName: string]: SimpleChange}) {
+    for (let change in changes) {
+      this[change] = changes[change] ? changes[change].currentValue : this[change];
+    }
+    if (!this.slider) {
+      this.slider = this.domElement.querySelectorAll('.jquilar-slider');
+      $(this.slider).slider({
+        stop: ( event, ui ) => {
+          this.stop.next(ui.value);
+        }
+      });
+    }
+    $(this.slider).slider({
+      orientation: this.orientation,
+      value: this.val,
+      step: this.step,
+    });
+  }
+}
+
+// jquery-ui datepicker
+@Component({
+  selector: 'jquilar-datepicker',
+  inputs: ['val'],
+  events: ['select'],
+  template: '<input type="text" class="jquilar-datepicker">'
+})
+
+export class jqUIlarDatePicker {
+  val: string;
+  select: EventEmitter<number>;
+  domElement: any;
+  datepicker: any;
+
+  constructor( @Inject(ElementRef) elementRef: ElementRef) {
+    this.domElement = elementRef.nativeElement;
+    this.select = new EventEmitter();
+    this.datepicker = undefined;
+    this.val = undefined;
+  }
+
+  ngAfterContentInit() {
+    if (!this.datepicker) {
+      this.datepicker = $(this.domElement).find('.jquilar-datepicker');
+      $(this.datepicker).datepicker({
+        onSelect: (dateText, ui) => {
+          this.select.next(dateText);
+        }
+      });
+      $(this.datepicker).datepicker("setDate", new Date());
+    }
+  }
+
+  ngOnChanges(changes: {[propertyName: string]: SimpleChange}) {
+    for (let change in changes) {
+      this[change] = changes[change] ? changes[change].currentValue : this[change];
+    }
+    if (!this.datepicker) {
+      this.datepicker = $(this.domElement).find('.jquilar-datepicker');
+    }
+    if (this.val) {
+      $(this.datepicker).val(this.val);
+    }
+    $(this.datepicker).datepicker({
+      onSelect: (dateText, ui) => {
+        this.select.next(dateText);
+      }
+    });
+  }
+}
